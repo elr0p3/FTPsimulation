cdcae4fb49eb972e72a71d76090a94640f3c577c
elr0p3 <ropefalora@gmail.com>
2021-03-24 17:09:38 +0100

    Added some client classes

---
+++ FTPclient/src/commands/ICommand.java
@@ -0,0 +1,28 @@
+package commands;
+
+public class ICommand {
+
+	// commands list
+	// ...
+	// ...
+
+	public ICommand () {
+
+	}
+
+	public void inputCommand () {
+
+	}
+
+	public String matchCommand (String user_command) {
+		switch (user_command) {
+			// case "":
+			// 	return "";
+			//
+			// case "";
+			// 	return "";
+		}
+		return "";
+	}
+
+}
---
+++ FTPclient/src/connection/CommandConnection.java
@@ -0,0 +1,12 @@
+package connection;
+
+import java.io.*;
+import java.net.*;
+
+public class CommandConnection {
+
+	public CommandConnection () {
+
+	}
+
+}
