3d1a207000d776b37ed676144ea0f146adb6621a
gabivlj <gabitriqui@gmail.com>
2021-03-30 05:06:16 +0100

    added PORT parsing and added more safety to LIST

--- FTPserver/rust_server/ftp_server/src/ftp/command.rs
+++ FTPserver/rust_server/ftp_server/src/ftp/command.rs
@@ -1,10 +1,11 @@
-use std::{convert::TryFrom, path::Path};
+use std::{convert::TryFrom, net::Ipv4Addr, path::Path};
 
 #[derive(Clone, Debug, PartialEq)]
 pub enum Command<'a> {
     /// To initiate any data transfer in active mode, the client must send this command.
     /// The first 4 bytes is the host IPv4 addr. and the rest is the port number
-    Port(u32, u16),
+    ///  h1,h2,h3,h4,p1,p2
+    Port(Ipv4Addr, u16),
 
     /// Pointer to string, which indicates the desired folder path
     /// ## Cases
@@ -19,10 +20,31 @@ fn expects_byte(byte: u8, expected_byte: u8, msg: &'static str) -> Result<(), &'
     Ok(())
 }
 
+fn ascii_to_u8(buff: &[u8]) -> Option<u8> {
+    if buff.len() > 3 {
+        return None;
+    }
+    let mut n: u16 = 0;
+    for byte in buff {
+        if n > 0 {
+            n *= 10;
+        }
+        let digit = (*byte as char).to_digit(10)?;
+        n += digit as u16;
+    }
+    if n > std::u8::MAX as u16 {
+        return None;
+    }
+    Some(n as u8)
+}
+
 impl<'a> TryFrom<&'a [u8]> for Command<'a> {
     type Error = &'static str;
 
     fn try_from(command: &'a [u8]) -> Result<Self, &'static str> {
+        if command.len() <= 2 {
+            return Err("Command is too short");
+        }
         expects_byte(
             command[command.len() - 1],
             b'\n',
@@ -37,8 +59,11 @@ impl<'a> TryFrom<&'a [u8]> for Command<'a> {
         match command[0] {
             // Possible commands = LIST
             b'L' => {
+                if command.len() <= 6 {
+                    return Err("invalid command length");
+                }
                 if &command[1..4] != b"IST" {
-                    return Err("invalid command, maybe you meant: `LIST`");
+                    return Err("Invalid command, maybe you meant: `LIST`?");
                 }
                 expects_byte(
                     command[4],
@@ -51,6 +76,53 @@ impl<'a> TryFrom<&'a [u8]> for Command<'a> {
                 let path = Path::new(path_str);
                 Ok(Command::List(path))
             }
+
+            b'P' => {
+                if command.len() <= 6 {
+                    return Err("invalid command length");
+                }
+                if &command[1..4] != b"ORT" {
+                    return Err("Invalid command, maybe you meant: `PORT`?");
+                }
+                expects_byte(
+                    command[4],
+                    b' ',
+                    "Expected space in between command and the rest.",
+                )?;
+
+                let mut ip_addr = [0u8; 4];
+                let mut port = [0u8; 2];
+                let mut byte_idx = 5;
+
+                // Parse IP + port
+                for i in 0..6 {
+                    let prev = byte_idx;
+                    while byte_idx < command.len() - 2 && command[byte_idx] != b',' {
+                        byte_idx += 1;
+                    }
+                    if i >= 4 {
+                        port[i - 4] =
+                            ascii_to_u8(&command[prev..byte_idx]).ok_or("Invalid port number")?;
+                    } else {
+                        ip_addr[i] =
+                            ascii_to_u8(&command[prev..byte_idx]).ok_or("Invalid IPv4 address")?;
+                    }
+                    byte_idx += 1;
+                }
+
+                // Check we reached the end of the command
+                if byte_idx != command.len() - 1 {
+                    return Err("Bad format of the `PORT` command");
+                }
+
+                // Try to get the IPv4
+                let ip = Ipv4Addr::from(ip_addr);
+
+                // This is the formula for getting the port number
+                let port: u16 = port[0] as u16 * 256 + port[1] as u16;
+                Ok(Command::Port(ip, port))
+            }
+
             _ => Err("invalid command"),
         }
     }
@@ -59,15 +131,32 @@ impl<'a> TryFrom<&'a [u8]> for Command<'a> {
 #[cfg(test)]
 mod test {
     use super::Command;
-    use std::{convert::TryFrom, path::Path};
+    use std::{convert::TryFrom, net::Ipv4Addr, path::Path};
 
     #[test]
     fn check_command_parsing_works() {
-        let tests = [(
-            "LIST ./test/test/test1.txt\r\n".as_bytes(),
-            Command::List(Path::new("./test/test/test1.txt")),
-            true,
-        )];
+        let tests = [
+            (
+                "LIST ./test/test/test1.txt\r\n".as_bytes(),
+                Command::List(Path::new("./test/test/test1.txt")),
+                true,
+            ),
+            (
+                "PORT 0,0,0,0,0,20\r\n".as_bytes(),
+                Command::Port(Ipv4Addr::new(0, 0, 0, 0), 20),
+                true,
+            ),
+            (
+                "PORT 255,255,100,100,40,20\r\n".as_bytes(),
+                Command::Port(Ipv4Addr::new(255, 255, 100, 100), 40 * 256 + 20),
+                true,
+            ),
+            (
+                "PORT 1,253,0,20,40,200\r\n".as_bytes(),
+                Command::Port(Ipv4Addr::new(1, 253, 0, 20), 40 * 256 + 200),
+                true,
+            ),
+        ];
         for test in tests.iter() {
             let (command_buff, expected_path, should_be_equal) = test;
             let command_try = Command::try_from(&command_buff[..]);
