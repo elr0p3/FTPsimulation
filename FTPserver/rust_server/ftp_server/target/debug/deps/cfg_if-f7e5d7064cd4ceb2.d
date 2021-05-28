14890c53e1b5789d715c9c6ac09bfa146b81d284
elr0p3 <ropefalora@gmail.com>
2021-05-28 22:01:18 +0200

    deleted old ftp server dir

--- FTPserver/rust_server/ftp_server/target/debug/deps/cfg_if-f7e5d7064cd4ceb2.d
+++
@@ -1,7 +0,0 @@
-/Users/gabivlj/programming/Java/FTPsimulation/FTPserver/rust_server/ftp_server/target/debug/deps/cfg_if-f7e5d7064cd4ceb2.rmeta: /Users/gabivlj/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs
-
-/Users/gabivlj/programming/Java/FTPsimulation/FTPserver/rust_server/ftp_server/target/debug/deps/libcfg_if-f7e5d7064cd4ceb2.rlib: /Users/gabivlj/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs
-
-/Users/gabivlj/programming/Java/FTPsimulation/FTPserver/rust_server/ftp_server/target/debug/deps/cfg_if-f7e5d7064cd4ceb2.d: /Users/gabivlj/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs
-
-/Users/gabivlj/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs:
--- FTPserver/rust_server/ftp_server/target/release/build/log-04cfbf2d7b40eb20/build_script_build-04cfbf2d7b40eb20.d
+++
@@ -1,5 +0,0 @@
-/Users/gabivlj/programming/Java/FTPsimulation/FTPserver/rust_server/ftp_server/target/release/build/log-04cfbf2d7b40eb20/build_script_build-04cfbf2d7b40eb20: /Users/gabivlj/.cargo/registry/src/github.com-1ecc6299db9ec823/log-0.4.14/build.rs
-
-/Users/gabivlj/programming/Java/FTPsimulation/FTPserver/rust_server/ftp_server/target/release/build/log-04cfbf2d7b40eb20/build_script_build-04cfbf2d7b40eb20.d: /Users/gabivlj/.cargo/registry/src/github.com-1ecc6299db9ec823/log-0.4.14/build.rs
-
-/Users/gabivlj/.cargo/registry/src/github.com-1ecc6299db9ec823/log-0.4.14/build.rs:
